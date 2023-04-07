a51a620fc9efb13c9c9eb36de26b974d901d92c5
Finley <finleyrosserdavies@gmail.com>
2023-04-07 16:07:23 +0100

    implemented basic Trackable base class

--- tkinter_experiments/object_observer.py
+++ tkinter_experiments/object_observer.py
@@ -3,36 +3,47 @@ import tkinter
 
 def notify_method_call(func):
     def wrapper(*args, **kwargs):
-        print(f"Calling {func.__name__}")
-        args[0].notify_observers(func.__name__, args[1:])
+        print(f"notifying observers: {func.__name__}(args={args}, kwargs={kwargs})")
+        args[0].notify_observers(func.__name__, args[1:] + tuple(kwargs))
         return func(*args, **kwargs)
+    return wrapper
 
 
 class Trackable:
     def __init__(self):
-        self.observers = []
+        self._observers = []
+        self.test_var = 0
 
     def __setattr__(self, key, value):
-        for observer in self.observers:
-            observer.notify(key, value)
+        self.__dict__[key] = value
+
+        if not key.startswith("_"):
+            self.notify_observers(key, value)
 
     def notify_observers(self, key, value):
-        for observer in self.observers:
+        print(f"notifying observers: {key} = {value}")
+        for observer in self._observers:
             observer.notify(key, value)
 
     @notify_method_call
     def add_observer(self, observer):
-        self.observers.append(observer)
+        self._observers.append(observer)
 
     @notify_method_call
     def remove_observer(self, observer):
-        self.observers.remove(observer)
+        self._observers.remove(observer)
+
+    @notify_method_call
+    def test(self, arg):
+        self.test_var = arg
+        print(f"test: {arg}")
 
 
 def main():
     window = tkinter.Tk()
 
     track = Trackable()
+    track.test("hello")
 
 
     window.mainloop()
